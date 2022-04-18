5b7f682b3f139199bd3a69784b0c62aa79ee891b
Yuvraj Singh Chauhan <yuvi.chauhan1990@gmail.com>
2022-04-18 19:39:20 +0530

    General testing bot
    Just to test the working of basic surfing of analogous answers to the FAQs

--- CoviBot NTCC.py
+++ CoviBot NTCC.py
@@ -1,3 +1,6 @@
+# This is a basic chatbot implementation surfing answers from wikipidea only
+# Basically it was for testing purpose
+
 from chatterbot import ChatBot
 import pyttsx3 as tts
 from tkinter import *
@@ -16,9 +19,9 @@ root.geometry("445x585")
 
 
 def speak(audio):
-    voices = engine.getProperty('voices')
+    voices = engine.getProperty("voices")
     engine.setProperty("voice", voices[1].id)
-    rate = engine.getProperty('rate')
+    rate = engine.getProperty("rate")
     engine.setProperty("rate", 180)
     engine.say(audio)
     engine.runAndWait()
@@ -45,9 +48,9 @@ def bot_will_respond():
     chatWindow.insert(END, "CoviBot : " + str(answer))
     messageWindow.delete(0, END)
     chatWindow.yview(END)
-    voices = engine.getProperty('voices')
+    voices = engine.getProperty("voices")
     engine.setProperty("voice", voices[1].id)
-    engine.getProperty('rate')
+    engine.getProperty("rate")
     engine.setProperty("rate", 160)
     engine.say(str(answer))
     engine.runAndWait()
@@ -70,9 +73,19 @@ photoL.pack(pady=3)
 
 frame = Frame(root)
 scrollbar = Scrollbar(frame, relief=RIDGE, bd=5)
-scrollbar2 = Scrollbar(frame, orient='horizontal', relief=RIDGE, bd=5)
-chatWindow = Listbox(frame, highlightthickness=3, yscrollcommand=scrollbar.set, xscrollcommand=scrollbar2.set,
-                     relief=RAISED, bg="gray88", fg="cyan4", width=500, height=15, font=("Bell MT", 13, "bold"))
+scrollbar2 = Scrollbar(frame, orient="horizontal", relief=RIDGE, bd=5)
+chatWindow = Listbox(
+    frame,
+    highlightthickness=3,
+    yscrollcommand=scrollbar.set,
+    xscrollcommand=scrollbar2.set,
+    relief=RAISED,
+    bg="gray88",
+    fg="cyan4",
+    width=500,
+    height=15,
+    font=("Bell MT", 13, "bold"),
+)
 scrollbar.pack(side=RIGHT, fill=Y, pady=4)
 scrollbar2.pack(side=BOTTOM, fill=X, pady=0)
 chatWindow.pack(side=LEFT, fill=BOTH, pady=4)
@@ -82,10 +95,27 @@ chatWindow.config(highlightbackground="cyan2", highlightcolor="cyan2")
 frame.pack()
 
 frame1 = Frame(root)
-Button = Button(frame1, bd=5, relief=RAISED, text="Enter", bg="light sea green", fg="black", height=1,
-                activebackground="cyan4", font=("BatmanForeverAlternate", 16), command=bot_will_respond)
-messageWindow = Entry(frame1, relief=GROOVE, highlightthickness=3, bg="White", fg="black", width=300,
-                      font=("Bell MT", 13, "bold"))
+Button = Button(
+    frame1,
+    bd=5,
+    relief=RAISED,
+    text="Enter",
+    bg="light sea green",
+    fg="black",
+    height=1,
+    activebackground="cyan4",
+    font=("BatmanForeverAlternate", 16),
+    command=bot_will_respond,
+)
+messageWindow = Entry(
+    frame1,
+    relief=GROOVE,
+    highlightthickness=3,
+    bg="White",
+    fg="black",
+    width=300,
+    font=("Bell MT", 13, "bold"),
+)
 Button.pack(side=RIGHT, fill=X, pady=4)
 messageWindow.pack(side=LEFT, fill=BOTH, pady=4)
 messageWindow.config(highlightbackground="cyan3", highlightcolor="cyan3")
