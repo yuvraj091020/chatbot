8ea4df76f48f4498560c70e482dbde238d86ba5d
Yuvraj Singh Chauhan <yuvi.chauhan1990@gmail.com>
2022-04-18 19:19:21 +0530

    Added comments

--- NTCC Project.py
+++ NTCC Project.py
@@ -8,11 +8,10 @@ import requests
 
 
 class ENGSM:
-    ISO_639_1 = 'en_core_web_sm'
+    ISO_639_1 = "en_core_web_sm"
 
 
-headers = {
-    "apikey": "c9f2b640-ff55-11eb-a01f-055218eaec09"}
+headers = {"apikey": "c9f2b640-ff55-11eb-a01f-055218eaec09"}  # used zenserp API
 
 bot = ChatBot("CoviBot", tagger_language=ENGSM)
 engine = tts.init()
@@ -25,9 +24,9 @@ root.geometry("445x585")
 
 
 def speak(audio):
-    voices = engine.getProperty('voices')
+    voices = engine.getProperty("voices")
     engine.setProperty("voice", voices[1].id)
-    rate = engine.getProperty('rate')
+    rate = engine.getProperty("rate")
     engine.setProperty("rate", 180)
     engine.say(audio)
     engine.runAndWait()
@@ -50,9 +49,7 @@ wishMe()
 def bot_will_respond():
     question = messageWindow.get()
     chatWindow.insert(END, "User :" + question)
-    parameters = (
-        ("q", question),
-    )
+    parameters = (("q", question),)
     answer, answer2 = "", ""
     try:
         answer2 = wikipedia.summary(question, sentences=2)
@@ -61,11 +58,13 @@ def bot_will_respond():
         print("No results found in wikipedia for:", question)
 
     try:
-        answer = requests.get('https://app.zenserp.com/api/v2/search', headers=headers, params=parameters)
-        res = answer.json()['organic']
+        answer = requests.get(
+            "https://app.zenserp.com/api/v2/search", headers=headers, params=parameters
+        )
+        res = answer.json()["organic"]
         for dta in res:
             try:
-                answer = dta['description']
+                answer = dta["description"]
                 break
             except Exception as e:
                 print(e)
@@ -86,9 +85,9 @@ def bot_will_respond():
     else:
         chatWindow.insert(END, "CoviBot  :" + str(answer))
     messageWindow.delete(0, END)
-    voices = engine.getProperty('voices')
+    voices = engine.getProperty("voices")
     engine.setProperty("voice", voices[1].id)
-    engine.getProperty('rate')
+    engine.getProperty("rate")
     engine.setProperty("rate", 160)
     engine.say(str(answer))
     engine.runAndWait()
@@ -111,9 +110,19 @@ photoL.pack(pady=3)
 
 frame = Frame(root)
 scrollbar = Scrollbar(frame, relief=RIDGE, bd=5)
-scrollbar2 = Scrollbar(frame, orient='horizontal', relief=RIDGE, bd=5)
-chatWindow = Listbox(frame, highlightthickness=3, yscrollcommand=scrollbar.set, xscrollcommand=scrollbar2.set,
-                     relief=RAISED, bg="gray88", fg="cyan4", width=500, height=15, font=("Bell MT", 13, "bold"))
+scrollbar2 = Scrollbar(frame, orient="horizontal", relief=RIDGE, bd=5)
+chatWindow = Listbox(
+    frame,
+    highlightthickness=3,
+    yscrollcommand=scrollbar.set,
+    xscrollcommand=scrollbar2.set,
+    relief=RAISED,
+    bg="gray88",
+    fg="cyan4",
+    width=500,
+    height=15,
+    font=("Bell MT", 13, "bold"),
+)
 scrollbar.pack(side=RIGHT, fill=Y, pady=4)
 scrollbar2.pack(side=BOTTOM, fill=X, pady=0)
 chatWindow.pack(side=LEFT, fill=BOTH, pady=4)
@@ -123,10 +132,27 @@ chatWindow.config(highlightbackground="cyan2", highlightcolor="cyan2")
 frame.pack()
 
 frame1 = Frame(root)
-Button = Button(frame1, bd=5, relief=RAISED, text="Enter", bg="light sea green", fg="black", height=1,
-                activebackground="cyan4", font=("BatmanForeverAlternate", 16), command=bot_will_respond)
-messageWindow = Entry(frame1, relief=GROOVE, highlightthickness=3, bg="White", fg="black", width=300,
-                      font=("Bell MT", 13, "bold"))
+Button = Button(
+    frame1,
+    bd=5,
+    relief=RAISED,
+    text="Enter",
+    bg="light sea green",
+    fg="black",
+    height=1,
+    activebackground="cyan4",
+    font=("BatmanForeverAlternate", 16),
+    command=bot_will_respond,
+)
+messageWindow = Entry(
+    frame1,
+    relief=GROOVE,
+    highlightthickness=3,
+    bg="White",
+    fg="black",
+    width=300,
+    font=("Bell MT", 13, "bold"),
+)
 Button.pack(side=RIGHT, fill=X, pady=4)
 messageWindow.pack(side=LEFT, fill=BOTH, pady=4)
 messageWindow.config(highlightbackground="cyan3", highlightcolor="cyan3")
